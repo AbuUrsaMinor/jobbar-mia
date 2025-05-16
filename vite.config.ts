51a3347207d721ae8208dc4c1d4f564a8c9e5449
Erik Johansson <erik.b.johansson@afconsult.com>
2025-05-16 14:50:48 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/jobbar-mia/', // Base path for GitHub Pages
 })
